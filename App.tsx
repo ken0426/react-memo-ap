066c143cb6f3b5fde73ae7cfc5c60895445dd29d
ken0426 <kenkaitixyou0426@gmail.com>
2023-01-08 23:15:47 +0900

    [feat] 画面全体のイメージを作成

--- App.tsx
+++ App.tsx
@@ -1,9 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View>
+        <View>
+          <Text>Memo App</Text>
+          <Text>ログアウト</Text>
+        </View>
+      </View>
+
+      {/* ------------ ここからリスト ------------ */}
+      <View>
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+          </View>
+          <View>
+            <Text>2020年12月24日 10:00</Text>
+          </View>
+        </View>
+        <View>
+          <Text>✗</Text>
+        </View>
+      </View>
+      {/* ------------ ここまでリスト ------------ */}
+
+      {/* ------------ 新規登録ボタン ------------ */}
+      <View>
+        <Text>+</Text>
+      </View>
+
       {/* eslint-disable */}
       <StatusBar style='auto' />
     </View>
