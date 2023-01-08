37b1dd25c7b4cf708b26775265ca07537d5fd11d
ken0426 <kenkaitixyou0426@gmail.com>
2023-01-09 00:25:18 +0900

    [feat] App.tsxのリファクタリング、コンポーネント化
    また、ESLintのエラーの解消

--- App.tsx
+++ App.tsx
@@ -1,35 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import AppBar from './src/components/AppBar';
+import CircleButton from './src/components/CircleButton';
+import MemoList from './src/components/MemoList';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.appBar}>
-        <View style={styles.appBarInner}>
-          <Text style={styles.appBarTitle}>Memo App</Text>
-          <Text style={styles.appBarRight}>ログアウト</Text>
-        </View>
-      </View>
-
-      {/* ------------ ここからリスト ------------ */}
-      <View>
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
-          </View>
-          <View>
-            <Text>✗</Text>
-          </View>
-        </View>
-      </View>
-      {/* ------------ ここまでリスト ------------ */}
-
-      {/* ------------ 新規登録ボタン ------------ */}
-      <View style={styles.circleButton}>
-        <Text style={styles.circleButtonLabel}>+</Text>
-      </View>
-
+      <AppBar />
+      <MemoList />
+      <CircleButton icon={'+'} />
       {/* eslint-disable */}
       <StatusBar style='auto' />
     </View>
@@ -40,67 +20,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F0F4F8',
-  },
-  appBar: {
-    width: '100%',
-    height: 104,
-    backgroundColor: '#467FD3',
-    justifyContent: 'flex-end',
-  },
-  appBarInner: {
-    alignItems: 'center',
-  },
-  appBarRight: {
-    position: 'absolute',
-    right: 19,
-    bottom: 16,
-    color: 'rgba(255, 255, 255, 0.8)',
-  },
-  appBarTitle: {
-    marginBottom: 8,
-    fontSize: 22,
-    lineHeight: 32,
-    color: '#fff',
-    fontWeight: 'bold',
-  },
-  memoListItem: {
-    backgroundColor: '#fff',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingVertical: 16,
-    paddingHorizontal: 19,
-    alignItems: 'center',
-    borderBottomWidth: 1,
-    borderColor: 'rgba(0,0,0, 0.15)',
-  },
-  memoListItemTitle: {
-    fontSize: 16,
-    lineHeight: 32,
-  },
-  memoListItemDate: {
-    fontSize: 12,
-    lineHeight: 16,
-    color: '#848484',
-  },
-  circleButton: {
-    backgroundColor: '#467FD3',
-    width: 64,
-    height: 64,
-    borderRadius: 32,
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'absolute',
-    right: 40,
-    bottom: 40,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 8 },
-    shadowOpacity: 0.25,
-    shadowRadius: 8, // iOS
-    elevation: 8, // Android
-  },
-  circleButtonLabel: {
-    color: '#fff',
-    fontSize: 40,
-    lineHeight: 40,
   },
 });
---
+++ src/components/CircleButton.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+type Props = {
+  icon: string;
+};
+
+const CircleButton = ({ icon }: Props) => {
+  return (
+    <View style={styles.circleButton}>
+      <Text style={styles.circleButtonLabel}>{icon}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  circleButton: {
+    backgroundColor: '#467FD3',
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    right: 40,
+    bottom: 40,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.25,
+    shadowRadius: 8, // iOS
+    elevation: 8, // Android
+  },
+  circleButtonLabel: {
+    color: '#fff',
+    fontSize: 40,
+    lineHeight: 40,
+  },
+});
+
+export default CircleButton;
