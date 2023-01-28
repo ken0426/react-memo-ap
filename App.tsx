2d521d3ba3ba7207e3ad29c5c51d00b316ef999c
ken0426 <kenkaitixyou0426@gmail.com>
2023-01-28 16:22:48 +0900

    メモの編集画面を実装

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
+import MemoEditScreen from './src/components/MemoEditScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoListScreen from './src/screens/MemoListScreen';
 
 export default function App() {
   return (
     // <MemoListScreen />
-    <MemoDetailScreen />
+    // <MemoDetailScreen />
+    <MemoEditScreen />
   );
 }
