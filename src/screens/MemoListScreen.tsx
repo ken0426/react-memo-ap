348df5371080eaf7271b9e90db000b4365c0b8e5
ken0426 <kenkaitixyou0426@gmail.com>
2023-02-04 00:34:20 +0900

    [feat] メモのリストをデータベースから受け取る処理を追加

--- src/screens/MemoListScreen.tsx
+++ src/screens/MemoListScreen.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Alert, StyleSheet, View } from 'react-native';
 import CircleButton from '../components/CircleButton';
 import LogOutButton from '../components/LogOutButton';
 import MemoList from '../components/MemoList';
+import firebase from 'firebase';
 
 const MemoListScreen = ({ navigation }: any) => {
   useEffect(() => {
@@ -10,6 +11,30 @@ const MemoListScreen = ({ navigation }: any) => {
       headerRight: () => <LogOutButton navigation={navigation} />,
     });
   }, []);
+
+  // データベースからリストのデータを受け取る処理
+  useEffect(() => {
+    const db = firebase.firestore();
+    const { currentUser } = firebase.auth();
+    let unsubscribe = () => {};
+    if (currentUser) {
+      try {
+        const ref = db
+          .collection(`users/${currentUser?.uid}/memos`)
+          .orderBy('updateAt', 'desc'); // 新しいものが一番上に来るようにレスポンスを貰えるようにするため「desc」を追加。
+        unsubscribe = ref.onSnapshot((snapshot) => {
+          snapshot.forEach((doc) => {
+            console.log(doc.id, doc.data());
+          });
+        });
+      } catch (error) {
+        Alert.alert('データの読み込みに失敗しました。');
+        return console.log(error);
+      }
+    }
+    return unsubscribe;
+  }, []);
+
   return (
     <View style={styles.container}>
       <MemoList navigation={navigation} />
